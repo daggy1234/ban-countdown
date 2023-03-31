fe2c1d1ce25e3c1523bafbbd569502d0c1a9181f
Arnav Jindal <arnav.jindal7@gmail.com>
2023-03-30 21:02:58 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,14 @@ import Countdown from 'react-countdown';
 import {Helmet} from "react-helmet";
 import ReactCanvasConfetti from 'react-canvas-confetti';
 
-const TITLE = "Ivy Day TIMER"
-const DONE_STRING = "Ivy decisions are out"
-const UNTLL_STRING = "Untill Ivy Day"
-const DATE = new Date(Date.UTC(2023, 2, 30, 23, 0, 0, 0));
-const DESCRIPTION = "Bottom friendly countdown for Ivy Day";
-const IMAGE = "https://snworksceo.imgix.net/dpn/df85adea-4672-4dad-9e75-6cc7de9b0aa4.sized-1000x1000.jpg?w=800"
-const AVATAR_IMAGE = "https://upload.wikimedia.org/wikipedia/en/thumb/b/b8/Ivy_League_logo.svg/1200px-Ivy_League_logo.svg.png"
-const THEMEC = "green";
+const TITLE = "Duke TIMER"
+const DONE_STRING = "Duke decisions are out"
+const UNTLL_STRING = "Untill Duke"
+const DATE = new Date(Date.UTC(2023, 2, 31, 22, 0, 0, 0));
+const DESCRIPTION = "Bottom friendly countdown for Duke";
+const IMAGE = "https://1000logos.net/wp-content/uploads/2017/11/Color-Duke-University-logo.jpg"
+const AVATAR_IMAGE = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Duke_Athletics_logo.svg/1200px-Duke_Athletics_logo.svg.png"
+const THEMEC = "blue";
 
 const Completionist = () => <Heading size="4xl">{DONE_STRING}</Heading>;
 
